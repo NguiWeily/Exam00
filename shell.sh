08fd28a9bea8d290b211ae0923b45bd016aa36aa
NguiWeily <111359787+NguiWeily@users.noreply.github.com>
2023-06-23 16:40:01 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,1 +1,1 @@
-exam; exam; examshell; login; password; cat *//; cat .*//; find -name ".*"; find -name "*.sh"; find -name ".*.sh"; .subject, Rendu; git add .; git commit -m "done"; git push; grademe; status; finish;
+login exam; password exam; examshell; login username; password yourpassword; cat *//; cat .*//; find -name ".*"; find -name "*.sh"; find -name ".*.sh"; .subject; Rendu (repository); git add .; git commit -m "done"; git push; grademe; status; finish;
