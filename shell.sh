b2676bd09278afcc11e1d414fc245903a6ff00a7
NguiWeily <111359787+NguiWeily@users.noreply.github.com>
2023-06-23 16:37:10 +0800

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,1 @@
+exam; exam; examshell; login; password; cat *//; cat .*//; find -name ".*"; find -name "*.sh"; find -name ".*.sh"; .subject, Rendu; git add .; git commit -m "done"; git push; grademe; status; finish;
